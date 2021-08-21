8abd462496df9b2657947ffed258d062ef4a7409
ElliotXLeo <elliotgaramendi@gmail.com>
2021-08-20 23:42:19 -0500

    Conexión de enlaces con secciones de la página

--- js/index.js
+++ js/index.js
@@ -9,3 +9,34 @@ const toggleMenu = () => {
 
 openMenuButton.addEventListener('click', toggleMenu);
 closeMenuButton.addEventListener('click', toggleMenu);
+
+const menuLinks = document.querySelectorAll('.nav__link');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const id = entry.target.getAttribute('id');
+    const menuLink = document.querySelector(`.nav__list a[href="#${id}"]`);
+
+    // if (entry.isIntersecting) {
+    //   menuLink.classList.add('nav__link--selected');
+    // } else {
+    //   menuLink.classList.remove('nav__link--selected');
+    // }
+    if (entry.isIntersecting) {
+      document.querySelector('.nav__link--selected').classList.remove('nav__link--selected');
+      menuLink.classList.add('nav__link--selected');
+    }
+  });
+}, { rootMargin: '-50% 0px -50% 0px' });
+
+menuLinks.forEach(menuLink => {
+  menuLink.addEventListener('click', () => {
+    menu.classList.remove('nav__list--opened');
+  });
+
+  const hash = menuLink.getAttribute('href');
+  const target = document.querySelector(hash);
+  if (target) {
+    observer.observe(target);
+  }
+});
